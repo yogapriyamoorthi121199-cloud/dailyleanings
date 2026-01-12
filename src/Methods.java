56fb2a5ff81c7271c2c2520b2aca44fe3bd313f4
yogapriya <yogapriyamoorthi121199@gmail.com>
2026-01-12 20:38:27 +0530

    first file

---
+++ src/Methods.java
@@ -0,0 +1,26 @@
+ class Methods {
+         public static void main(String[] args) {
+             String name = "Wishes";
+             System.out.println(name);
+             Goodmorning(); /// method calling
+             Goodafternoon();
+             Goodevening();
+             Goodnight();
+         }
+         public  static void Goodmorning() ///  method defining
+         {
+             System.out.println("Goodmorning");
+         }
+         public static void Goodafternoon()
+         {
+             System.out.println("Goodafternoon");
+         }
+         public static void Goodevening()
+         {
+             System.out.println("Goodevening");
+         }
+         public static void Goodnight()
+         {
+             System.out.println("Goodnight");
+         }
+}
