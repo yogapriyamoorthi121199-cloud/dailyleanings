a9789e7de8523421dd3a83b0448b73f2d927f614
yogapriya <yogapriyamoorthi121199@gmail.com>
2026-01-18 08:01:10 +0530

    first file

---
+++ src/Methods1.java
@@ -0,0 +1,34 @@
+ class Methods1 {
+    public static void main(String[] args){
+        Addition();
+        Subtration();
+        Multiplication();
+        Division();
+        Modulardivision();
+    }
+    public static void Addition()
+    {
+     int x=7,z=9;
+     System.out.println(x+z);
+    }
+    public static void Subtration()
+    {
+     int r=8,g=8;
+     System.out.println(r-g);
+    }
+    public static void Multiplication()
+     {
+      int d=9,m=12;
+      System.out.println(d*m);
+     }
+     public static void Division()
+     {
+      int f=45,h=6;
+      System.out.println(f/h);
+     }
+     public static void Modulardivision()
+     {
+       int b=43,t=12;
+       System.out.println(b%t);
+     }
+}
