e85e70f0263406a0360fc76f7c858cdca4593325
yogapriya <yogapriyamoorthi121199@gmail.com>
2026-02-26 12:25:58 +0530

    first file

---
+++ src/ifcondition.java
@@ -0,0 +1,13 @@
+ class ifcondition {
+     public static void main(String[] args) {
+
+         int marks = 70;
+         if (marks >= 70) {
+
+             System.out.println("pass");
+         } else
+         {
+             System.out.println("fail");
+         }
+     }
+ }
---
+++ src/oddoreven.java
@@ -0,0 +1,13 @@
+class oddoreven {
+    public static void main(String[] args) {
+        int a = 54;
+        if(a % 2 == 0) {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+
+        }
+    }
+}
